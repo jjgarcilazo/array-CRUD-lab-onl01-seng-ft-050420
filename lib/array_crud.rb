005ac75a3577c19d0cd9f683aa49760e448c6524
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-06 02:14:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+ sandwich ["ham" , "Egg"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  ["ham" , "Egg"]
+   << "Cheese"
 end
 
 def add_element_to_start_of_array(array, element)
